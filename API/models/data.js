59761f3f1935fea7d6dde9253dd1a0df426d5ed9
habumugisha ami des jeunes <habajeunes2@gmail.com>
2019-08-23 19:25:29 +0200

    ft As user should be able to sign up [starts #168065056]

---
+++ API/models/data.js
@@ -0,0 +1,29 @@
+export const users= [
+    {   id : 1,
+        firstName : "kamana",
+        lastName : "kamanamana",
+        email : "kamana@gmail.com",
+        password :"1234567890",
+        adress: "Rwanda kigali",
+        biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        occupation : "freelancer",
+        expertise: "BackEnd developer"
+    },
+    {
+        id: 2,
+    firstName : "John",
+     lastName : "johnPeter",
+     email : "john5@gmail.com",
+     password :"1234567890",
+     adress: "Rwanda kigali",
+     biography: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+     occupation : "freelancer",
+     expertise: "BackEnd developer"
+ }
+
+ ];
+
+ 
+ export const sessions = [];
+
+ export const reviews = [];
