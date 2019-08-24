2b70956ec0c251537d741f6d79c06cf51b54f0c6
habumugisha ami des jeunes <habajeunes2@gmail.com>
2019-08-24 12:57:51 +0200

    ft As user should be able to log in after creating account so that can access web platform resource [starts #168065400]

--- API/routes/userRoutes.js
+++ API/routes/userRoutes.js
@@ -3,12 +3,13 @@ import express from 'express';
 import Users from '../controllers/userController';
 let router = express.Router();
 
-// import User from '../models/user';
-router.get('/home', Users.homeView )
-// router.post('/auth/login', Users.userlogin)
-router.post('/auth/signup', Users.userSignUp )
-router.get('/auth/users', Users.getusers )
-router.get('/auth/users/:id', Users.specificuser )
+
+// router.get('/home', Users.homeView )
+router.post('/auth/login', Users.userlogin)
+// router.post('/auth/signup', Users.userSignUp )
+// router.get('/auth/users', Users.getusers )
+// router.get('/auth/users/:id', Users.specificuser )
+
 
 
 export default router;
