e5b464825599b5a03174044c14c1c79000481853
habumugisha ami des jeunes <habajeunes2@gmail.com>
2019-08-30 10:41:03 +0200

    ch as developer should update UI for user get it interactive [finishes #168212145]

--- UI/static/js/accept.js
+++ UI/static/js/accept.js
@@ -9,14 +9,16 @@ child.innerHTML="session declined!!";
 // const color = 'red';
 // parent.removeChild(child);
 console.log(child);
-alert("session refused!!!");
+alert("session declined!!!");
 };
 
 function accept() {
+    
     const section = document.getElementsByTagName("section");
     const parent =document.getElementById("usermainId");
     const child = document.getElementById("section");
     child.style.color = 'green';
     alert("Thanks! this session accepted.");
-    console.log(section);
-};
+
+    
+}
