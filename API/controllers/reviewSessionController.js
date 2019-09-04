9bd99492bdc387c04d1d9fe79b1c9a0b09bba7b3
habumugisha ami des jeunes <habajeunes2@gmail.com>
2019-09-04 15:12:23 +0200

    ch(dependeces): add dependeces
    create travis file
    integrete travis dependence
    Updating READMEfile
    [finishes #168289126]

--- API/controllers/reviewSessionController.js
+++ API/controllers/reviewSessionController.js
@@ -1,62 +1,68 @@
-import sessionMiddleware from '../middleware/sessionMiddleware'
-import jwt from 'jsonwebtoken';
-import { users, mentors, sessions, reviews } from '../models/data';
+// import jwt from 'jsonwebtoken';
+// import sessionMiddleware from '../middleware/sessionMiddleware';
+import { sessions, reviews } from '../models/data';
 
-    
+
 export default class ReviewSessions {
-    
-  static createReview(req, res,) {
-        const session = sessions.find(s => s.sessionId === parseInt(req.params.sessionId,10) && (req.userInfo.user.role === "mentor"))
-        if(req.userInfo.role !== "mentor") 
-        //unauthorized status code
-        return res.status(401).json({
-          message: 'your are not allowed only mentor is allowed to create review'
-        })
+  static createReview(req, res) {
+    const session = sessions.find((s) => s.sessionId === parseInt(req.params.sessionId, 10) && (req.userInfo.user.role === 'mentor'));
+    if (req.userInfo.role !== 'mentor')
+    // unauthorized status code
+    {
+      return res.status(401).json({
+        message: 'your are not allowed only mentor is allowed to create review',
+      });
+    }
 
-        if(!session) return res.status(404).json({
-          status: 404,
-            message:`session with ID ${req.params.sessionId} you passed is not found!`   
-         })
-         if(session){
-         const review = {
-                          
-                          sessionId: sessions.length + 1,
-                          mentorId :sessions.length + 1,
-                          menteeId : req.userInfo.user.id,
-                          score: req.body.score,
-                          menteeFullName: `${req.userInfo.user.firstName} ${req.userInfo.user.lastName}`, 
-                          remark: req.body.remark
-                          
-                        };
-                    reviews.push(review);
-                        res.status(200).json({
-                           status :200,
-                            data: review
-                            
-                        })
-               } 
-    
-          };
+    if (!session) {
+      return res.status(404).json({
+        status: 404,
+        message: `session with ID ${req.params.sessionId} you passed is not found!`,
+      });
+    }
+    if (session) {
+      const review = {
 
-  static deleteReview (req,res){
-          const session = sessions.find(r => r.sessionId === parseInt(req.params.sessionId,10) && (req.userInfo.user.role === "admin"))
-          if(req.userInfo.role !== "admin")
-          //unauthorized status code 
-           return res.status(401).json({
-          message: 'your are not allowed only admin is allowed to delete this review'
-        })
-          
-          if(!session) return res.status(404).json({
-            status: 404,
-              message:`session with ID ${req.params.sessionId} you passed is not found`   
-           }) 
-          if(session) sessions.splice(session, 1) 
-          res.status(200).json({
-            status: 200,
-            data: {
-            message:`Review with id ${req.params.sessionId} successfully deleted!!`
-            }
-          })
-        }; 
- 
-    } 
+        sessionId: sessions.length + 1,
+        mentorId: sessions.length + 1,
+        menteeId: req.userInfo.user.id,
+        score: req.body.score,
+        menteeFullName: `${req.userInfo.user.firstName} ${req.userInfo.user.lastName}`,
+        remark: req.body.remark,
+
+      };
+      reviews.push(review);
+      res.status(200).json({
+        status: 200,
+        data: review,
+
+      });
+    }
+  }
+
+  static deleteReview(req, res) {
+    const session = sessions.find((r) => r.sessionId === parseInt(req.params.sessionId, 10) && (req.userInfo.user.role === 'admin'));
+    if (req.userInfo.role !== 'admin')
+    // unauthorized status code
+    {
+      return res.status(401).json({
+        message: 'your are not allowed only admin is allowed to delete this review',
+      });
+    }
+
+    if (!session) {
+      return res.status(404).json({
+        status: 404,
+        message: `session with ID ${req.params.sessionId} you passed is not found`,
+      });
+    }
+
+    if (session) sessions.splice(session, 1);
+    res.status(200).json({
+      status: 200,
+      data: {
+        message: `Review with id ${req.params.sessionId} successfully deleted!!`,
+      },
+    });
+  }
+}
--- API/controllers/sessionController.js
+++ API/controllers/sessionController.js
@@ -1,78 +1,79 @@
 import jwt from 'jsonwebtoken';
 import { users, mentors, sessions } from '../models/data';
 
-    
+
 export default class Sessions {
-    // user should request a session to mentor
-    static requestSession(req, res) {
-    
-         const session = {
-                          sessionId: sessions.length + 1,
-                          mentorId : req.body.mentorId,
-                          menteeId : req.userInfo.user.id,
-                          questions: req.body.questions,
-                          menteeEmail: req.userInfo.user.email,
-                          status: "pending", 
-                        };
-                    sessions.push(session);
-                        res.status(200).json({
-                            message: 'session requested successfully!',
-                            data: session,
-                            
-                        })
-                        };
+  // user should request a session to mentor
+  static requestSession(req, res) {
+    const session = {
+      sessionId: sessions.length + 1,
+      mentorId: req.body.mentorId,
+      menteeId: req.userInfo.user.id,
+      questions: req.body.questions,
+      menteeEmail: req.userInfo.user.email,
+      status: 'pending',
+    };
+    sessions.push(session);
+    res.status(200).json({
+      message: 'session requested successfully!',
+      data: session,
 
-       // mentor accepting menotorship request session
-    static acceptSession (req, res) {
-        const session = sessions.find(s => s.sessionId === parseInt(req.params.sessionId,10))
-       
-        if(!session) return res.status(404).json({
-            
-                status: 404,
-                message: 'session with ID you type is not found',
-                })
-                 
-       res.status(200).json({ 
-        status: 200,
-        data: {
-            sessionId: req.params.sessionId,
-            mentorId : req.body.mentorId,
-            menteeId : req.userInfo.user.id,
-            questions: req.body.questions,
-            menteeEmail: req.userInfo.user.email,
-            status: "Accepted"
-          }
-        })
-    
-        }
-      
-      // mentor should be able to decline session request mentorship
-    static declineSession(req, res) {
-        const session = sessions.find(s => s.sessionId === parseInt(req.params.sessionId,10))
-           if(!session) return res.status(404).json({
-             status: 404,
-               message:`session with ID ${req.params.sessionId}you passed is not found!`   
-            })
+    });
+  }
 
-     res.status(200).json({
-        status:200,
-        data: {
-            sessionId: req.params.sessionId,
-            mentorId : req.body.mentorId,
-            menteeId : req.userInfo.user.id,
-            questions: req.body.questions,
-            menteeEmail: req.userInfo.user.email,
-            status: "rejected"
-          }
-        })
-      }
+  // mentor accepting menotorship request session
+  static acceptSession(req, res) {
+    const session = sessions.find((s) => s.sessionId === parseInt(req.params.sessionId, 10));
+
+    if (!session) {
+      return res.status(404).json({
+
+        status: 404,
+        message: 'session with ID you type is not found',
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      data: {
+        sessionId: req.params.sessionId,
+        mentorId: req.body.mentorId,
+        menteeId: req.userInfo.user.id,
+        questions: req.body.questions,
+        menteeEmail: req.userInfo.user.email,
+        status: 'Accepted',
+      },
+    });
+  }
+
+  // mentor should be able to decline session request mentorship
+  static declineSession(req, res) {
+    const session = sessions.find((s) => s.sessionId === parseInt(req.params.sessionId, 10));
+    if (!session) {
+      return res.status(404).json({
+        status: 404,
+        message: `session with ID ${req.params.sessionId}you passed is not found!`,
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      data: {
+        sessionId: req.params.sessionId,
+        mentorId: req.body.mentorId,
+        menteeId: req.userInfo.user.id,
+        questions: req.body.questions,
+        menteeEmail: req.userInfo.user.email,
+        status: 'rejected',
+      },
+    });
+  }
 
 
-      // get sessions
-    static getSession(req, res) {
-        res.status(200).json({
-          sessions
-        })
-      }             
-                     
-     } 
+  // get sessions
+  static getSession(req, res) {
+    res.status(200).json({
+      sessions,
+    });
+  }
+}
